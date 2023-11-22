2910e89781212b9066c67f5fad14ad480b04e5a1
Gabitosz <gabriel.zdravkovici313@gmail.com>
2023-11-23 00:24:32 +0200

    refactor code

--- HW-19.playground/Contents.swift
+++ HW-19.playground/Contents.swift
@@ -1,80 +1,66 @@
 import Foundation
 import CryptoKit
 
-let url = "https://dogapi.dog/api/facts/"
-
-// MARK: Base Network Request
+// MARK: Network Request
 
 func getData(urlRequest: String) {
-    let urlRequest = URL(string: urlRequest)
-    guard let url = urlRequest else { return }
+    guard let url = URL(string: urlRequest) else { return }
+    
     URLSession.shared.dataTask(with: url) { data, response, error in
-        
-        if error != nil {
-            if let error = error {
-                print("Error: \(String(describing: error.localizedDescription))")
-            }
+        if let error = error {
+            print("Error: \(error.localizedDescription)")
         } else if let response = response as? HTTPURLResponse, response.statusCode == 200 {
-            print("Done! Status Code: \(response.statusCode)")
-            guard let data = data else { return }
-            let dataAsString = String(data: data, encoding: .utf8)
-            guard let finalData = dataAsString else { return }
-            
-            if let jsonData = finalData.data(using: .utf8),
-               let jsonObject = try? JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any] {
-                if let header = jsonObject["facts"] {
-                    if let fact = jsonObject["facts"] {
-                        let stringValue = String(describing: fact)
-                        let cleanStringValue = stringValue.replacingOccurrences(of: "(", with: "")
-                            .replacingOccurrences(of: ")", with: "")
-                            .trimmingCharacters(in: .whitespacesAndNewlines)
-                        print("Random fact about Dogs: \(cleanStringValue)")
-                    } else {
-                        print("Key 'facts' doesn't found")
-                    }
-                } else {
-                    do {
-                        if let jsonObject = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any],
-                           let data = jsonObject["data"] as? [String: Any],
-                           let results = data["results"] as? [[String: Any]],
-                           let character = results.first,
-                           let name = character["name"] as? String {
-                            print("Character Name: \(name)")
-                        } else {
-                            print("Failed to extract character name")
-                        }
-                    } catch {
-                        print("Error decoding JSON: \(error)")
-                    }
-//                    else {
-//                       print("JSON serialization failed")
-//                   }
-                }
-                             
-            } else {
-                if let response = response as? HTTPURLResponse {
-                    print("Status Code: \(response.statusCode). Failed to fetch data. Verify the URL.")
-                }
+            print("Done! Status Code: \(response.statusCode) -> ", terminator: "")
+            if let data = data, let dataAsString = String(data: data, encoding: .utf8) {
+                parseData(dataAsString)
             }
+        } else if let response = response as? HTTPURLResponse {
+            print("Status Code: \(response.statusCode). Failed to fetch data. Verify the URL.")
         }
     }.resume()
 }
 
-getData(urlRequest: url)
+func parseData(_ dataAsString: String) {
+    guard let jsonData = dataAsString.data(using: .utf8),
+          let jsonObject = try? JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any] else {
+        print("JSON serialization failed")
+        return
+    }
 
-// MARK: 🌟🌟🌟
+    if let fact = jsonObject["facts"] {
+        processFact(fact)
+    } else if let data = jsonObject["data"] as? [String: Any],
+              let results = data["results"] as? [[String: Any]],
+              let character = results.first,
+              let name = character["name"] as? String {
+        print("Marvel Character Name: \(name)")
+    } else {
+        print("Key 'facts' or 'data' doesn't found")
+    }
+}
+
+func processFact(_ fact: Any) {
+    let stringValue = String(describing: fact)
+    let cleanStringValue = stringValue.replacingOccurrences(of: "(", with: "")
+        .replacingOccurrences(of: ")", with: "")
+        .trimmingCharacters(in: .whitespacesAndNewlines)
+    print("Random fact about Dogs: \(cleanStringValue)")
+}
+
+// MARK: MD5 Hash Function
 
 func MD5(string: String) -> String {
     let digest = Insecure.MD5.hash(data: Data(string.utf8))
-    
-    return digest.map {
-        String(format: "%02hhx", $0)
-    }.joined()
+    return digest.map { String(format: "%02hhx", $0) }.joined()
 }
 
-MD5(string: "1b89768d6681ac50fb8a9c470d083935e7f677cdbcbb2452d6645b4338f0e732373eb6647")
+// MARK: URL Objects
 
-getData(urlRequest: "https://gateway.marvel.com:443/v1/public/characters/1011136?ts=1&apikey=cbb2452d6645b4338f0e732373eb6647&hash=dbf085cf11af35cfb528412c9a11d58d")
+let url = "https://dogapi.dog/api/facts/"
+getData(urlRequest: url)
+
+let marvelUrl = "https://gateway.marvel.com:443/v1/public/characters/1011136?ts=1&apikey=cbb2452d6645b4338f0e732373eb6647&hash=dbf085cf11af35cfb528412c9a11d58d"
+getData(urlRequest: marvelUrl)
 
 
 
